3e36f4bf792a52edf7f3407485cefff7f77e9051
Robert Batorowski <Robert.Batorowski@redwood.com>
2025-10-18 22:15:21 +0200

    refactor: Use Testcontainers instead of H2 for testing
    This commit replaces the H2 in-memory database with Testcontainers for integration testing.
    
    This change provides a more realistic testing environment by using a real PostgreSQL database in a Docker container.
    
    The following changes were made:
    
    - Removed the H2 dependency from the `build.gradle.kts` file.
    - Removed the `application-test.yml` file.
    - Updated the `ApplicationTests.java` to use `@Testcontainers` and a PostgreSQL container.
    - Removed the `TestcontainersConfiguration` class and configured the container directly in the `ApplicationTests` class.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 
     runtimeOnly("org.postgresql:postgresql")
 
-    testImplementation("com.h2database:h2")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.security:spring-security-test")
